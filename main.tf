ec309ea48d67ac3ed0d8577a0f7ba15e345cf214
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2024-02-09 15:03:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "3.89.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = "StorageRG"
+    storage_account_name = "taskboardstorageniko"
+    container_name = "taskboardcontainer"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
